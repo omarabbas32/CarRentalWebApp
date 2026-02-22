a3b13826c6fd5c95c5e86d765b34cc803d84f8f5
omarabbas32 <abbas8989omar@gmail.com>
2026-02-22 04:00:34 +0200

    adding exception handling

--- backend/Application/Behaviors/ValidationBehavior.cs
+++ backend/Application/Behaviors/ValidationBehavior.cs
@@ -19,20 +19,22 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if (!_validators.Any())
+        if (_validators.Any())
         {
-            return await next();
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Count != 0)
+                throw new Common.Exceptions.ValidationException(failures);
         }
-
-        var context = new ValidationContext<TRequest>(request);
-        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-        var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
-
-        if (failures.Count != 0)
-        {
-            throw new ValidationException(failures);
-        }
-
         return await next();
     }
 }
