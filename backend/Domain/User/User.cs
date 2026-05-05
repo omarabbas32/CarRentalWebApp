abe1c6819554738e0c1f7dea413cd6cc6c1a4c79
omarabbas32 <abbas8989omar@gmail.com>
2026-05-05 13:42:54 +0300

    Update user domain models

--- backend/Domain/User/User.cs
+++ backend/Domain/User/User.cs
@@ -38,22 +38,5 @@ namespace Domain.User
         public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
     }
 
-    public class UserVerification
-    {
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public virtual User User { get; set; } = null!;
-
-        public string? DriverLicenseNumber { get; set; }
-        public string? DriverLicenseState { get; set; }
-        public DateTime? DriverLicenseExpiryDate { get; set; }
-        public string? DriverLicenseFrontImageUrl { get; set; }
-        public string? DriverLicenseBackImageUrl { get; set; }
-        public VerificationStatus DriverLicenseStatus { get; set; }
-
-        public string? GovernmentIdNumber { get; set; }
-        public GovernmentIdType? GovernmentIdType { get; set; }
-        public string? GovernmentIdImageUrl { get; set; }
-        public VerificationStatus GovernmentIdStatus { get; set; }
-    }
+   
 }
